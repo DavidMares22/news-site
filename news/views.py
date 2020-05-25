cefa690fb047941b3a6c43e8230f8083d7f40996
damr <damr18@gmail.com>
2020-05-25 15:44:25 -0500

    News model

--- news/views.py
+++ news/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import News
 
 
 def index(request):
-    return render(request,'news/index.html')
+    items = News.objects.all()
+    return render(request,'news/index.html',{'items':items})

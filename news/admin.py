ce8aed085bcc7676716008630a9f02617d9c26e5
damr <damr18@gmail.com>
2020-05-28 13:47:36 -0500

    category model, model admin, bootstrap style

--- news/admin.py
+++ news/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
 from .models import News
+from .models import Category
 # Register your models here.
 
-admin.site.register(News)
+
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'category','is_published')
+    list_editable = ('is_published',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+   
+
+admin.site.register(News,NewsAdmin)
+admin.site.register(Category,CategoryAdmin)

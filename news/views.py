8ffffa81a223cfec289a381edd9029d35c5daed2
damr <damr18@gmail.com>
2020-06-01 09:40:42 -0500

    simple tag , {% includes %}

--- news/views.py
+++ news/views.py
@@ -5,13 +5,11 @@ from .models import News,Category
 
 def index(request):
     items = News.objects.all()
-    categories = Category.objects.all()
-    return render(request,'news/index.html',{'items':items, 'categories':categories})
+    return render(request,'news/index.html',{'items':items})
 
 
 def get_category(request,id):
     
     news = News.objects.filter(category_id = id)
-    categories = Category.objects.all()
     category = Category.objects.get(pk=id)
-    return render(request,'news/detail.html',{'category':category,'news':news, 'categories':categories})
+    return render(request,'news/detail.html',{'category':category,'news':news})

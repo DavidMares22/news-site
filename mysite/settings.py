88510a77422cbd0b47ac4f462439ec6836c5474b
damr <damr18@gmail.com>
2020-06-30 09:27:46 -0500

    ckeditor

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ckeditor',
+    'ckeditor_uploader',
     'news.apps.NewsConfig',
 ]
 
@@ -119,10 +121,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    
-]
+
+
+STATICFILES_DIRS = (
+  os.path.join(BASE_DIR, 'static'),
+)
+
+# STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
@@ -134,3 +139,8 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_PASSWORD = ""
+
+
+
+
+CKEDITOR_UPLOAD_PATH = "uploads/"
--- news/admin.py
+++ news/admin.py
@@ -1,14 +1,21 @@
 from django.contrib import admin
 from .models import News
 from .models import Category
-# Register your models here.
 
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+from django import forms
 
+class NewsAdminForm(forms.ModelForm):
+    content = forms.CharField(widget=CKEditorUploadingWidget())
 
+    class Meta:
+        model = News
+        fields = '__all__'
     
 
 
 class NewsAdmin(admin.ModelAdmin):
+    form = NewsAdminForm
     list_display = ('id', 'title', 'category','is_published')
     list_editable = ('is_published',)
     
